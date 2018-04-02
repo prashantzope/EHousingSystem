26aae8779aaa0f027a03a2ec00d37338ff8b820f
prashant <prashant.zope@derivco.co.za>
2018-04-02 16:47:48 +0530

    test

--- deploy.cmd
+++ deploy.cmd
@@ -95,6 +95,8 @@ call :SelectNodeVersion
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install --production  
+  call :ExecuteCmd !NPM_CMD! npm install @angular/cli  
+  
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
