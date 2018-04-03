2e9c9a4b94c97e5ee7c54d29f16479d643760eed
prashant <prashant.zope@derivco.co.za>
2018-04-03 12:03:36 +0530

    deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -51,6 +51,7 @@ goto Deployment
 
 :: Utility Functions
 :: -----------------
+echo %DEPLOYMENT_TEMP%
 
 :SelectNodeVersion
 
@@ -88,21 +89,37 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
-:: 1. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
+
+
+:: 1. KuduSync to DEPLOYMENT_TEMP
+call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TEMP%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 2. Select node version
 call :SelectNodeVersion
-
+ 
 :: 3. Install npm packages
-IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
-  pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install --production
+IF EXIST "%DEPLOYMENT_TEMP%\package.json" (
+  pushd "%DEPLOYMENT_TEMP%"
+  call :ExecuteCmd !NPM_CMD! install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
+)
+ 
+:: 4. Angular Prod Build
+IF EXIST "%DEPLOYMENT_TEMP%/.angular-cli.json" (
+echo Building App in %DEPLOYMENT_SOURCE%…
+pushd "%DEPLOYMENT_TEMP%"
+call :ExecuteCmd !NPM_CMD! ng build --prod
+IF !ERRORLEVEL! NEQ 0 goto error
+popd
+)
+
+
+:: 5. KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
 )
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
