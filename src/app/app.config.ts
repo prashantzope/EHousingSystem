42dcdb346008d29688132f3b75a742c466e3439b
prashant <prashant.zope@derivco.co.za>
2018-06-01 14:46:07 +0530

    update api

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,8 @@
 export class AppSettings {
     
      
-          public static API_REGISTER_ENDPOINT='http://localhost:59609/api/';
+     // public static API_REGISTER_ENDPOINT='http://localhost:59609/api/';
+          public static API_REGISTER_ENDPOINT = 'https://ehmsapi.azurewebsites.net/api/";
     
     
     }
